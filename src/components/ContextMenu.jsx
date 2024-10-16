f3b7f9c652f8cfac2ff7468ee0f8e6a85c86a565
Moazzam <moazzam.ali6659@gmail.com>
2024-10-16 22:04:36 +0530

    Add custom right click context menu and also add delete row functionality

--- src/components/ContextMenu.jsx
+++ src/components/ContextMenu.jsx
@@ -1,8 +1,21 @@
-const ContextMenu = () => {
+const ContextMenu = ({ menuPosition, setMenuPosition, setExpenses, rowId }) => {
+  if (!menuPosition.left) return
   return (
-    <div className='context-menu'>
-      <div>Edit</div>
-      <div>Delete</div>
+    <div className='context-menu' style={{ ...menuPosition }}>
+      <div
+        onClick={() => {
+          console.log('Editing')
+          setMenuPosition({})
+        }}>
+        Edit
+      </div>
+      <div
+        onClick={() => {
+          setExpenses((prevState) => prevState.filter((expense) => expense.id !== rowId))
+          setMenuPosition({})
+        }}>
+        Delete
+      </div>
     </div>
   )
 }
